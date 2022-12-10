e5146808b451454f53a1e31282fd7dc7df3600ce
coderhero5 <abdulxakimtojiyev2004@gmail.com>
2022-12-10 13:32:57 +0500

    java script

---
+++ Js/main.js
@@ -0,0 +1,9 @@
+let son = prompt( );
+if (son > 90)
+console.log('budjet');
+else if(son > 70 && son < 90)
+console.log('contract');
+else if (son > 50 && son < 70)
+console.log('supern contract')
+else if (son < 50)
+console.log('Battar bol kirolmading')
